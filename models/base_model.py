c91a0ceb9bd1e892268743d7fc891215d5d5db80
HafsaMAR <marouche.hafsa@gmail.com>
2023-10-11 15:28:13 +0100

    first commit

--- models/base_model.py
+++ models/base_model.py
@@ -10,36 +10,39 @@ from datetime import datetime
 
 class BaseModel():
     """" Parent class""" 
-    # id = str(uuid4())   
-    def __init__(self):
-        self.id = str(uuid4())  
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
-    # @property
-    # def id(self):
-    #     return self.id
-    
-    # @id.setter
-    # def id(self, value):
-    #     self.id = value
+
+    def __init__(self, *arg, **kwargs):
+        if kwargs:
+            for k, w in kwargs.items():
+                if k == '__class__':
+                    continue
+                elif k in ['created_at', 'updated_at']:
+                    w = datetime.now().strptime(w, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, k, w)
+        else:
+            self.id = str(uuid4())  
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+
 
     
     def save(self):
         self.updated_at = datetime.now()
 
+
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
+
+
     def to_dict(self):
-        dictionary = self.__dict__
+        dictionary = self.__dict__.copy()
         dictionary['id'] = self.id
         dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return self.__dict__
-
-# model = BaseModel()
-# print(model.created_at)
+        dictionary['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dictionary['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return dictionary
 
 
